14b69034ea357916d2db72f048ded168c27d3e81
Guligaga <glggboko@gmail.com>
2025-10-28 22:12:53 +0200

    feat: add speech recognition filters

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
-import { effects, maps } from './data/index.js';
+import { effects, maps, TRANSLATIONS } from './data/index.js';
 import { bosses } from './data/bosses.js';
 import { EARTH_SHIFT } from './data/constants.js';
 import { toUpperCaseFirst, uniqueByKeys, sortByKeys } from './util/index.js';
+import { VoiceInput } from './voice-input/voiceInput.js';
 
 const BREAKPOINTS = {
   sm: 640,
@@ -33,6 +34,11 @@ export const App = {
   drawerOpen: true,
   startX: 0,
 
+  voiceInput: new VoiceInput(TRANSLATIONS),
+  voiceListening: false,
+  voiceRecognitionResult: null,
+  displayingTranscript: '',
+
   onMounted() {
     const savedTheme = localStorage.getItem('theme');
     this.autenticTheme = savedTheme === 'autentic';
@@ -41,12 +47,13 @@ export const App = {
       this.isDesktop = window.innerWidth >= BREAKPOINTS.lg;
       this.drawerOpen = !this.isDesktop;
     });
+
+    this.initVoiceInput();
   },
 
   filterMaps() {
     document.documentElement.setAttribute('data-theme', this.autenticTheme ? 'autentic' : 'light');
     this.showResult = false;
-    console.log('Filtering maps with', this.selectedBoss, this.selectedEarthShift);
     if (!this.selectedBoss || !this.selectedEarthShift) {
       this.filteredMaps = this.maps;
       return;
@@ -71,8 +78,6 @@ export const App = {
 
     const uniqueLocations = uniqueByKeys(locations, 'type', 'effect');
     this.mapLocations = sortByKeys(uniqueLocations, ['type', 'effect']);
-
-    console.log('Filtered Maps:', JSON.parse(JSON.stringify(this.filteredMaps)));
   },
 
   setResultMaps(location) {
@@ -98,6 +103,7 @@ export const App = {
     localStorage.setItem('theme', this.autenticTheme ? 'autentic' : 'default');
   },
 
+  // Slider
   onPointerDown(e) {
     this.startX = e.clientX;
   },
@@ -108,6 +114,69 @@ export const App = {
       this.drawerOpen = true;
     } else if (deltaX < -80 && this.drawerOpen) {
       this.drawerOpen = false;
+    }
+  },
+
+  // Voice Input
+  initVoiceInput() {
+    let voiceRecognitionResult = null;
+
+    this.voiceInput.onStart = () => {
+      this.voiceRecognitionResult = null;
+      this.displayingTranscript = '';
+    };
+
+    this.voiceInput.onResult = (result) => {
+      if (!this.voiceListening) return;
+
+      voiceRecognitionResult = result;
+      this.displayingTranscript = this.voiceInput.transcript;
+    };
+
+    this.voiceInput.onEnd = () => {
+      this.voiceRecognitionResult = voiceRecognitionResult;
+      this.displayingTranscript = '';
+
+      if (this.voiceRecognitionResult) {
+        let result = this.maps;
+        const location = { type: null, effect: null };
+        this.voiceRecognitionResult.matches.forEach((match) => {
+          const [category, key] = match.key.split('.');
+          switch (category) {
+            case 'bosses':
+              result = result.filter((map) => map.boss === key);
+              this.selectedBoss = this.bosses.find((b) => b.name === key);
+              this.filterMaps();
+              break;
+            case 'earthShifts':
+              result = result.filter((map) => map.earthShift === key);
+              this.selectedEarthShift = this.earthShifts.find((es) => es.key === key);
+              this.filterMaps();
+              break;
+            case 'locations':
+              location.type = key;
+              break;
+            case 'effects':
+              const effect = key === 'none' ? null : key;
+              location.effect = effect;
+              break;
+          }
+        });
+        if (location.type) {
+          this.setResultMaps(location);
+        }
+      }
+    };
+  },
+
+  toggleListening() {
+    this.voiceInput.toggle();
+    this.voiceListening = this.voiceInput.listening;
+  },
+
+  vibrate(pattern = 50) {
+    if (navigator.vibrate) {
+      navigator.vibrate(pattern);
     }
   },
 };
